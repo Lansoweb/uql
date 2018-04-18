dca335451d9fe873ca7ab80a15340dc03fcb200c
Leandro Silva <lansoweb@hotmail.com>
2018-04-18 13:20:53 -0300

    First zend-db builder version, tests and coverage

--- src/BuilderInterface.php
+++ src/BuilderInterface.php
@@ -7,6 +7,42 @@ use Psr\Http\Message\ServerRequestInterface;
 
 interface BuilderInterface
 {
+    // Logical
+    const OP_NOT = '$not';
+    const OP_IN = '$in';
+    const OP_NOT_IN = '$nin';
+    const OP_LIKE = '$like';
+    const OP_OR = '$or';
+    const OP_AND = '$and';
+    const OP_NULL = '$null';
+    const OP_NOT_NULL = '$nnull';
+
+    const OP_LOGIC = [
+        self::OP_NOT,
+        self::OP_IN,
+        self::OP_NOT_IN,
+        self::OP_LIKE,
+        self::OP_OR,
+        self::OP_AND,
+        self::OP_NULL,
+        self::OP_NOT_NULL,
+    ];
+
+    // Conditional
+    const OP_GREATER = '$gt';
+    const OP_GREATER_EQUAL = '$gte';
+    const OP_LESS = '$lt';
+    const OP_LESS_EQUAL = '$lte';
+    const OP_BETWEEN = '$bt';
+
+    const OP_CONDITIONAL = [
+        self::OP_GREATER,
+        self::OP_GREATER_EQUAL,
+        self::OP_LESS,
+        self::OP_LESS_EQUAL,
+        self::OP_BETWEEN
+    ];
+
     public function fromRequest(ServerRequestInterface $request);
     public function fromParams(array $query, array $hint = []);
 }
